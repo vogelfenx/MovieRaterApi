fc993be941ea570e405eebf97137362beaf96268
Kirill Fedyakov <kfedyakov@easirun.de>
2020-05-05 00:35:16 +0200

    adding of models

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from django.contrib.auth.models import User as auth_User
+from django.core.validators import MaxLengthValidator, MinValueValidator
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(_("Movie"), max_length=32)
+    description = models.TextField(_("Description"), max_length=360)
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(auth_User, on_delete=models.CASCADE)
+    stars = models.IntegerField(_("stars"), validators=[MinValueValidator(1), MaxLengthValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'movie'))
+        index_together = (('user', 'movie'))
