86938f7074d144fd916a2e31743e2c7591c044be
Kirill Fedyakov <kfedyakov@easirun.de>
2020-05-06 09:35:32 +0200

    api viewset method added (MovieViewSet)

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from django.contrib.auth.models import User
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
@@ -8,6 +11,32 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        
+        if 'stars' in request.data:
+
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(id=1)
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                message = "Rating updated"
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                message = "Rating created"
+
+            serializer = RatingSerializer(rating, many=False)
+            response = {'message': message, 'result': serializer.data}
+            return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            response = {'message': 'You need to provide stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
