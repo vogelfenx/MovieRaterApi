23010677db16bb888859f156584b97a5c47a0577
Kirill Fedyakov <kfedyakov@easirun.de>
2020-05-06 09:34:47 +0200

    bug fixing : MaxValueValidator instead of MaxLengthValidator

--- app/api/models.py
+++ app/api/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User as auth_User
-from django.core.validators import MaxLengthValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import gettext as _
 
@@ -12,7 +12,7 @@ class Movie(models.Model):
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(auth_User, on_delete=models.CASCADE)
-    stars = models.IntegerField(_("stars"), validators=[MinValueValidator(1), MaxLengthValidator(5)])
+    stars = models.IntegerField(_("stars"), validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     class Meta:
         unique_together = (('user', 'movie'))
