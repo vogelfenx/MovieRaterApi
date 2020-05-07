c244b59761354e085de470b1abc45d6c670d6466
Kirill Fedyakov <kfedyakov@easirun.de>
2020-05-07 21:39:36 +0200

    working on frontend; data fetching

--- app/frontend-app/movie-rater-web/src/App.js
+++ app/frontend-app/movie-rater-web/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import MovieList from './components/movie-list'
+import MovieDetails from './components/movie-details'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  state = {
+    movies: [],
+    selectedMovie: null
+  }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/api/movies/', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Token 691aff3fe3c45bb695a91b5f26b52948cf545a60'
+      }
+    }).then( resp => resp.json())
+    .then( res => this.setState({movies: res}))
+    .catch( error => console.log(error))
+  }
+
+  movieClicked = movie => {
+    this.setState({selectedMovie: movie})
+  }
+
+  render() {
+    return (
+      <div className="App">
         <h1>Movie Rater</h1>
-    </div>
-  );
+
+        <div className="layout">
+          <MovieList movies={this.state.movies} movieClicked={this.movieClicked}/>
+          <MovieDetails movie={this.state.selectedMovie}/>
+        </div>
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
