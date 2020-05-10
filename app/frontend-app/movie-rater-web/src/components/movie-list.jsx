fbe4c4d5783e230feb8a03501dff1a62065fc7a1
Kirill Fedyakov <kfedyakov@easirun.de>
2020-05-10 02:42:12 +0200

    added fontawesome icons for edit / delete

--- app/frontend-app/movie-rater-web/src/components/movie-list.jsx
+++ app/frontend-app/movie-rater-web/src/components/movie-list.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+var FontAwesome = require('react-fontawesome');
 
 function MovieList(props) {
 
@@ -10,9 +11,15 @@ function MovieList(props) {
         <div>
             { props.movies.map( movie => {
                 return (
-                    <h3 key={movie.id} onClick={movieClicked(movie)}>
-                        {movie.title}
-                    </h3>
+                    <div className="movieList">
+                        <h3 key={movie.id} onClick={movieClicked(movie)}>
+                            {movie.title}
+                        </h3>
+                        <div className="movieList__edit-update">
+                            <FontAwesome name="edit" className="movieList__update" />
+                            <FontAwesome name="trash" className="movieList__trash" />
+                        </div>
+                    </div>
                 )
             })}
         </div>
