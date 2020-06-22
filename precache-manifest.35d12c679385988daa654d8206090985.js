90a16168b147d172eb3f1e5045c2e88da4b6b421
Kirill Fedyakov <kfedyakov@easirun.de>
2020-06-22 18:01:48 +0200

    Updates

---
+++ precache-manifest.35d12c679385988daa654d8206090985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e665792d3d87f592c22c9626fe5d612",
+    "url": "/MovieRaterApi/index.html"
+  },
+  {
+    "revision": "b432f0f66093cc9cda81",
+    "url": "/MovieRaterApi/static/css/main.f60efade.chunk.css"
+  },
+  {
+    "revision": "52e70d64dce2232c05f8",
+    "url": "/MovieRaterApi/static/js/2.8a3e28a3.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/MovieRaterApi/static/js/2.8a3e28a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b432f0f66093cc9cda81",
+    "url": "/MovieRaterApi/static/js/main.69d7ddf3.chunk.js"
+  },
+  {
+    "revision": "a10609d2a98a68710e34",
+    "url": "/MovieRaterApi/static/js/runtime-main.77ad4e4d.js"
+  }
+]);
